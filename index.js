76b0be86f9103bb742a09dc51f76e17982eca696
Mouon <ahemsapsldk@gmail.com>
2023-10-05 13:13:34 +0900

    server.js 수정, search.js 추가 (더미데이터로 하드코딩) index.js 추가

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-
 const express = require('express');
 const path = require('path');
-const { performSearch } = require('./search');
+const { performSearch } = require('./search'); // 검색 로직 가져오기
 
 const app = express();
 const PORT = 8800;
@@ -24,15 +23,11 @@ app.get('/contact', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'contact.html'));
 });
 
+// 검색을 위한 API 엔드포인트
 app.get('/api/search', (req, res) => {
   const { query } = req.query;
-  const results = performSearch(query);
+  const results = performSearch(query); // 검색 함수 호출
   res.json({ results });
-});
-
-app.get('/api/message', (req, res) => {
-  const message = 'This is a sample message from the API.';
-  res.json({ message });
 });
 
 app.listen(PORT, () => {
